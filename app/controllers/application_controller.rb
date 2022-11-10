16ab1d12f2b9fe95a6c3f9fe2233490b4ccbccca
codertinie <martin.munyao@moringaschool.com>
2022-11-10 14:18:05 +0300

    Added get requests to the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,33 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/songs' do
+    songs = Song.all
+    songs.to_json
+  end
+
+  get '/songs/:id' do
+    song = Song.find(params[:id])
+    song.to_json
+  end
+
+  get '/albums' do
+    albums = Album.all
+    albums.to_json
+  end
+
+  get '/albums/:id' do
+    album = Album.find(params[:id])
+    album.to_json
+  end
+
+  get '/users' do
+    users = User.all
+    users.to_json
+  end
+
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
 end

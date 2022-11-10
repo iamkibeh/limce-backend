22b8bf3d2e74e040e360b33f97bf3638ac3d79e7
waikau maina <ericmaina.dev@gmail.com>
2022-11-10 12:44:19 +0300

    created columns for the reviews table

--- db/migrate/20221110094058_create_reviews_table.rb
+++ db/migrate/20221110094058_create_reviews_table.rb
@@ -1,4 +1,9 @@
 class CreateReviewsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :comment
+      t.integer :song_id
+      t.integer :user_id
+    end
   end
 end

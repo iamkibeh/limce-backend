e1c240e043216b17c12f84b42222ccb7f6cb4343
waikau maina <ericmaina.dev@gmail.com>
2022-11-10 15:09:06 +0300

    created the post controller for the reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here ywywyw
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # Add your routes here 
+
+  post "/reviews" do
+    user = User.find_or_create_by(name: params[:user_name])
+    song = Song.find(params[:review_data][:song_id])
+    review = Review.create(comment: params[:review_data][:comment], user_id: user.id, song_id: song.id)
+    review.to_json
   end
 
 end

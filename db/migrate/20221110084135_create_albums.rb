9768c088233776fb78343dddcf2f33eae44773e3
codertinie <martin.munyao@moringaschool.com>
2022-11-10 12:03:49 +0300

    Added column data to the migrations

--- db/migrate/20221110084135_create_albums.rb
+++ db/migrate/20221110084135_create_albums.rb
@@ -1,4 +1,9 @@
 class CreateAlbums < ActiveRecord::Migration[6.1]
   def change
+    create_table :albums do |t|
+      t.string :name
+      t.integer :artist_id
+      t.string :image_url
+    end
   end
 end
--- db/migrate/20221110084146_create_songs.rb
+++ db/migrate/20221110084146_create_songs.rb
@@ -1,4 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[6.1]
   def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :album_id
+      t.string :iframe_url
+    end
   end
 end
